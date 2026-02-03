296011ad6f72a116d6c342cf0608e6ef8d1bfbd7
CortoGyt <corto.gayetun@gmail.com>
2026-02-03 13:48:45 +0100

    Fix bonus logic

--- app/services.py
+++ app/services.py
@@ -1,5 +1,6 @@
 def compute_bonus(salary: float) -> float:
     if salary < 0:
         raise ValueError("Salary must be positive")
-    # ERREUR VOLONTAIRE
-    return salary * 0.2
+    if salary < 50000:
+        return salary * 0.05
+    return salary * 0.10
